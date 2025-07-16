87587ee169b6e73601e9f433570e3bd214ac97fc
Loc-La Cong <conglocwinner@gmail.com>
2025-07-17 01:26:59 +0700

    Update uart_pkg.sv

--- UART_IP_Verification_using_UVM/vip/uart_vip/uart_pkg.sv
+++ UART_IP_Verification_using_UVM/vip/uart_vip/uart_pkg.sv
@@ -2,9 +2,9 @@
 // Project       : UART VIP
 //=============================================================================
 // Filename      : uart_pkg.sv
-// Author        : Huy Nguyen
+// Author        : La Cong Loc
 // Company       : NO
-// Date          : 20-Dec-2021
+// Date          : 30-June-2021
 //=============================================================================
 // Description   : 
 //
